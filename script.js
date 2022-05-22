c6fac626314bec2991017aa440f9133e6a0decef
Justin Rivera <justin_rivera9@outlook.com>
2022-05-22 10:12:52 -0600

    added message options object

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+// Gets random number between 0 and what will be the length of each property
 const getRandomNum = (num) => {
     return Math.floor(Math.random() * num)
 };
 
-console.log(getRandomNum(5))
+// Object containing message options
+let messagesOptions {
+    accuracy: ['25%', '50%', '75%', '100%'],
+    player: ['LeBron James', 'Nikola Jokic', 'Steph Curry', 'Jordan Poole', 'Jayson Tatum'],
+    award: ['MVP', 'Most Improved Player of the year', '6th Man of the year', 'bum of the year']
+};
+
+//Final message will be pushed into this variable
+let finalMessage = [];
